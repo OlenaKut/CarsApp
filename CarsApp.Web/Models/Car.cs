fb7d79c6ecb70748fade6596ab7575905ccec05b
OlenaKut <cundrochka@gmail.com>
2025-05-16 11:40:29 +0200

    Added Model and Service

---
+++ CarsApp.Web/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsApp.Web.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ CarsApp.Web/Services/CarService.cs
@@ -0,0 +1,26 @@
+﻿using CarsApp.Web.Models;
+
+namespace CarsApp.Web.Services
+{
+    public class CarService
+    {
+        List<Car> cars =
+            [
+            new Car { Id = 11, Make = "Toyota", Model = "Camry", Year = 2020, Color = "Blue" },
+            new Car { Id = 32, Make = "Honda", Model = "Civic", Year = 2019, Color = "Red" },
+            new Car { Id = 3, Make = "Ford", Model = "Mustang", Year = 2021, Color = "Black" },
+            new Car { Id = 423, Make = "Chevrolet", Model = "Malibu", Year = 2018, Color = "White" },
+            ];
+
+        public Car[] GetAllCars() => cars.OrderBy(c => c.Make).ToArray();
+
+        public Car GetCarById(int id) => cars.Single(c => c.Id == id);
+
+        public void AddCar(Car car)
+        {
+            car.Id = cars.Count < 0 ? 1 : cars.Max(e => e.Id) + 1;
+            cars.Add(car);
+        }
+
+    }
+}
