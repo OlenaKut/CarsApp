cd3d1eaee405c41a739511a98c248e807a7ff174
OlenaKut <cundrochka@gmail.com>
2025-05-16 11:47:42 +0200

    Added Controller

---
+++ CarsApp.Web/Controllers/CarsControlles.cs
@@ -0,0 +1,46 @@
+﻿using CarsApp.Web.Models;
+using CarsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsApp.Web.Controllers
+{
+    public class CarsControlles : Controller
+    {
+        static CarService carService = new CarService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var cars = carService.GetAllCars();
+            return View(cars);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost("create")]
+        public IActionResult Create(Car car)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            carService.AddCar(car);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("details/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            var car = carService.GetCarById(id);
+            if (car == null)
+                return NotFound();
+            return View(car);
+        }
+
+    }
+}
